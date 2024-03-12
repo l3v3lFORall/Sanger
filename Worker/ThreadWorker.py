ff89f8ed48909cb7715a0b181273489b172e5194
Yang <uidq9589@v01.net>
2024-03-12 10:31:42 +0800

    Refactor:任务编排重构:0

---
+++ Worker/ThreadWorker.py
@@ -0,0 +1,10 @@
+class tWorker():
+    def __init__(self) -> None:
+        pass
+    def CreateTask(self) -> None:
+        # Task即一个Module的Manager，也即一个线程
+        pass
+    def CreateLayer(self) -> None:
+        # 每个Layer包含一至多个Task
+        pass
+    
--- run.py
+++
@@ -1,7 +0,0 @@
-from core import PluginManager
-
-
-a = PluginManager.MyManager()
-a.loadConf("Android.yaml")
-# a.loadPlugin("loadTarget")
-a.workByQueue()
