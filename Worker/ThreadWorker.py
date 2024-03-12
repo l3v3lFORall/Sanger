83bec8ab93a6b9544fea3fb99059fcec18713ae2
Yang <uidq9589@v01.net>
2024-03-12 10:49:22 +0800

    Update:ThreadWorker 创建结构:0

--- Worker/ThreadWorker.py
+++ Worker/ThreadWorker.py
@@ -1,5 +1,10 @@
 class tWorker():
+    # ThreadWorker模块日志统一格式：
+    # 
     def __init__(self) -> None:
+        pass
+    def getConfig(self) -> None:
+        # 调用Loader加载配置文件
         pass
     def CreateTask(self) -> None:
         # Task即一个Module的Manager，也即一个线程
@@ -7,4 +12,9 @@ class tWorker():
     def CreateLayer(self) -> None:
         # 每个Layer包含一至多个Task
         pass
-    
+    def LayerStart(self) -> None:
+        # 按层次启动任务
+        pass
+    def setOutsider(self) -> None:
+        # 运行结果向外部调用提供
+        pass
